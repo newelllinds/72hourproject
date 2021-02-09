cd429e4694f235610838fdc0c364c0bd54b4816e
Kquanisha <kdixon8709@yahoo.com>
2021-02-08 21:15:27 -0500

    Updated Open Weather file

--- src/components/openWeather/openWeather.jsx
+++ src/components/openWeather/openWeather.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import'./import
+import React, {useState} from 'react';
 
+const OpenWeather = (props) => {
+    const [weather, setWeather] =useState ([])
+        function fetchResults() {
+            const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${props.location.coordinates.latitude}&lon=${props.location.coordinates.longitude}&appid=2432b7764de16436f1494396f912f854`
+            console.log (URL)
+            fetch(URL)
 
-const openWeather = () =>{
-    return{
-
+    
+                .then((response) => response.json())
+                .then((json) => {
+                    setWeather(json)
+                    console.log(json)})
     }
-}
+
+    return (
+        <div>
+            <button onClick={fetchResults}>Current Weather</button>
+        </div>
+    );
+};
+
+export default OpenWeather;
