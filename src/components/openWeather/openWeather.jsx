a77bc3790be7f2e4408d83c0a62421b764564215
Perla Brown <62082149+PerlaBrownCreative@users.noreply.github.com>
2021-02-09 21:33:37 -0500

    updated

--- src/components/openWeather/openWeather.jsx
+++ src/components/openWeather/openWeather.jsx
@@ -4,9 +4,10 @@ import { ButtonToggle } from 'reactstrap';
 
 const OpenWeather = (props) => {
     const [weather, setWeather] = useState ([])
-        
+      
+    
     function fetchResults() {
-            const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${props.location.coordinates.latitude}&lon=${props.location.coordinates.longitude}&appid=2432b7764de16436f1494396f912f854`
+            const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${props.location.coordinates.latitude}&lon=${props.location.coordinates.longitude}&${units=metric}&appid=2432b7764de16436f1494396f912f854`
             
             fetch(URL)
                 .then((response) => response.json())
@@ -19,13 +20,26 @@ const OpenWeather = (props) => {
         // fetchResults();
     }, []);
 
+    const oppositeUnit = unit === "C" ? "F" : "C";
 
-        
+    const convert = () => {
+        if (unit === "C") {
+            const newT = temp * 1.8 + 32;
+            setTemp(Math.round(newT));
+            setUnit(oppositeUnit);
+        }
+
+        if (unit === "F") {
+            const newT = ((temp - 32) * 5) / 9;
+            setTemp(Math.round(newT));
+            setUnit(oppositeUnit);
+        }
+    };
+
 
     return (
         <div>
             <button onClick={fetchResults}>Current Weather</button>
-        
             <p>
                 <ButtonToggle color="secondary">{weather.temp}</ButtonToggle>
             </p>
